7e41925246ecf0ac8528b6ccb1423a83419d9bc1
kavin <kavin.srinivasan2@gmail.com>
2026-07-26 00:50:25 +0530

    Add: network-delay-time - submission-1

---
+++ Data Structures & Algorithms/network-delay-time/submission-1.cpp
@@ -0,0 +1,48 @@
+class Solution {
+   public:
+    using Edge = pair<int, int>;   // {neighbor, weight}
+    using State = pair<int, int>;  // {distance, node}
+
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        // build graph
+        vector<vector<Edge>> graph(n + 1);
+
+        for (const vector<int>& time : times) {
+            const int src = time[0];
+            const int dest = time[1];
+            const int weight = time[2];
+
+            graph[src].emplace_back(dest, weight);
+        }
+
+        // arr[node] = delay;
+        vector<int> delay(n + 1, INT_MAX);
+        // [delay,node]
+        priority_queue<State, vector<State>, greater<State>> pq;
+
+        delay[k] = 0;
+        pq.emplace(0, k);
+
+        while (!pq.empty()) {
+            auto [cur_delay, node] = pq.top();
+            pq.pop();
+
+            if (cur_delay > delay[node]) continue;
+
+            for (const auto& [nei, weight] : graph[node]) {
+                if (cur_delay + weight < delay[nei]) {
+                    delay[nei] = cur_delay + weight;
+                    pq.emplace(delay[nei], nei);
+                }
+            }
+        }
+
+        int res = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (delay[i] == INT_MAX) return -1;
+            res = max(res, delay[i]);
+        }
+
+        return res;
+    }
+};
